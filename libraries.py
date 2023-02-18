e6c03c480619f29523ce6d8712c0c269f5643beb
dermot2701 <125708435+dermot2701@users.noreply.github.com>
2023-02-19 07:31:25 +1100

    Delete libraries.py

--- libraries.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from keras.models import Sequential
-from keras.layers import Dense
